bb4b0c4d316f9cb475e7a2f0c0175eb3cef6e8d9
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-06-21 17:18:31 +0300

    adding a form

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,3 +1,21 @@
-export default function MealItemForm() {
-  return ()
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+export default function MealItemForm(props) {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
 }
